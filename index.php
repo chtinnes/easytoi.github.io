f1325bb49224b52a732b15d8840d5b96c6f0537d
David T <mail@davidtheobald.de>
2018-01-03 22:59:02 +0100

    test php

--- index.php
+++ index.php
@@ -197,7 +197,7 @@
 
     <footer>
       <div class="container">
-        <p>&copy; 2017 Start Bootstrap. All Rights Reserved.</p>
+        <p>&copy; 2017 <?php echo hello ?>. All Rights Reserved.</p>
         <ul class="list-inline">
           <li class="list-inline-item">
             <a href="#">Privacy</a>
