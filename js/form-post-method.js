32b8267a7ba0dd5394fd7ae614848811ba4a2734
David T <mail@davidtheobald.de>
2018-01-05 14:29:55 +0100

    Revert "add jquery script for post form method to prevent url redirect"
    This reverts commit e39671fabd2b34c5c9d82f56fb34d4c3f2d358ab.

--- js/form-post-method.js
+++
@@ -1,9 +0,0 @@
-function sendForm() {
-	jQuery.ajax({
-		url : 'http://www.davidtheobald.de/easytoi-form-script.php',
-		type : 'POST',
-		success : function(results) {
-			jQuery(".result").html(results);
-		}
-	});
-}
